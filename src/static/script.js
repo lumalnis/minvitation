1e4cd8e1a4d7e6b4597c95c3c79ee609c83c629d
lumalnis <luciamalnis@gmail.com>
2024-07-24 18:26:22 -0300

    ourStory done, starting notes

--- src/static/script.js
+++ src/static/script.js
@@ -1,3 +1,13 @@
+/**
+@param {string} selector
+*/
+
+const hideElements = selector => {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(el => el.style.display = 'none');
+};
+
+
 const getRemainTime = deadline => {
     let now = new Date(),
         remainTime = (new Date(deadline) - now + 1000) / 1000;
@@ -15,17 +25,34 @@ const getRemainTime = deadline => {
     };
 };
 
-const countdown = (deadline, daysElem, hoursElem, minutesElem, secondsElem, messageT, daysT, hoursT, minutesT, secondsT) => {
+const countdown = (deadline, daysElem, hoursElem, minutesElem, secondsElem, messageT, daysT, hoursT, minutesT, secondsT, errorT) => {
     const daysEl = document.getElementById(daysElem);
     const hoursEl = document.getElementById(hoursElem);
     const minutesEl = document.getElementById(minutesElem);
     const secondsEl = document.getElementById(secondsElem);
     const messageEl = document.getElementById(messageT);
-    const overEls = document.querySelectorAll('.over');
+    //const overEls = document.querySelectorAll('.over');
     const daysTx = document.getElementById(daysT);
     const hoursTx = document.getElementById(hoursT);
     const minutesTx = document.getElementById(minutesT);
     const secondsTx = document.getElementById(secondsT);
+    const errorEl = document.getElementById(errorT);
+
+    //Validación de fecha
+    const deadlineDate = new Date(deadline);
+    if (isNaN(deadlineDate.getTime())) {
+        //ENGLISH
+        /*
+         if (errorEl) { 
+             errorEl.innerHTML = "Invalid Date.";
+             hideElements('.over');}
+         */
+        //SPANISH
+        if (errorEl) {
+            errorEl.innerHTML = "Fecha inválida.";
+            hideElements('.over');
+        }
+    }
 
     const timerUpdate = setInterval(() => {
         let t = getRemainTime(deadline);
@@ -34,7 +61,8 @@ const countdown = (deadline, daysElem, hoursElem, minutesElem, secondsElem, mess
 
         if (t.remainTime <= 1) {
             clearInterval(timerUpdate);
-            overEls.forEach(el => el.style.display = 'none');
+            // overEls.forEach(el => el.style.display = 'none');
+            hideElements('.over');
             const now = new Date();
             const eventDate = new Date(deadline);
 
@@ -51,7 +79,7 @@ const countdown = (deadline, daysElem, hoursElem, minutesElem, secondsElem, mess
             } else {
                 messageEl.innerHTML = `El evento finalizó.`;
             }
-            
+
         } else {
 
             daysEl.innerHTML = t.remainDays;
@@ -72,13 +100,57 @@ const countdown = (deadline, daysElem, hoursElem, minutesElem, secondsElem, mess
             hoursTx.innerHTML = t.remainHours === 1 ? `Hora` : 'Horas';
             minutesTx.innerHTML = t.remainMinutes === 1 ? `Minuto` : 'Minutos';
             secondsTx.innerHTML = t.remainSeconds === 1 ? `Segundo` : 'Segundos';
-
         }
     }, 1000)
 };
 
-countdown('Jul 21 2024 14:05:50 GMT-0300', 'days', "hours", "minutes", "seconds", 'message', 'daysText', 'hoursText', 'minutesText', 'secondsText');
+countdown('Jul 25 2024 14:05:50 GMT-0300', 'days', "hours", "minutes", "seconds", 'message', 'daysText', 'hoursText', 'minutesText', 'secondsText', 'errorText');
 
-/*FORMATO (Jul 24 2024 10:32:53 GMT-0300) GMT-0300 es hora Argentina*/
+//FORMATO (Jul 24 2024 10:32:53 GMT-0300) GMT-0300 es hora Argentina
+
+//REALIZAR DOS SCRIPTS UNO EN SPANISH OTRO EN ENGLISH Y VER SI PUEO AGREGAR TRADUCIR PAGINA
 
 
+/*NOTES*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    let createBox = document.getElementsByClassName('createBox')[0];
+    let notes = document.getElementsByClassName('notes')[0];
+    let input = document.getElementById('userInput');
+    let i = 0;
+
+    input.addEventListener('keydown', content);
+
+    document.getElementById("create").addEventListener("click", function () {
+        createBox.style.display = "block";
+        input.focus();
+    });
+
+    function content(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            divStyle(input.value);
+            input.value = "";
+            createBox.style.display = "none";
+        }
+    }
+
+    function color() {
+        let randomColors = ["#c2ff3d", "#ffd256", "#0b0b0b"];
+        if (i > randomColors.length - 1) { i = 0; }
+        return randomColors[i++];
+    }
+
+    function divStyle(text) {
+        let div = document.createElement("div");
+        div.className = 'note';
+        div.innerHTML = '<div class="details"><h3>' + text + '</h3></div>';
+
+        //para eliminar loa notes
+        //div.addEventListener("dblclick", function() { div.remove(); });
+
+        div.setAttribute('style', 'background:' + color() + ';');
+
+        notes.appendChild(div);
+    }
+});
